81ee0cf475a8f50ca1398b4007ea474e92a4737c
Tiyasha Mallick <20051855@kiit.ac.in>
2023-05-03 12:55:42 +0530

    Newwwwwwwwwwwwwww

--- app.py
+++ app.py
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import streamlit as st
-import cv2
-import numpy as np
-import keras
-from tensorflow.keras.models import load_model
-from streamlit_drawable_canvas import st_canvas
-import matplotlib.pyplot as plt
-
-model = load_model('final.h5')
-
-
-def preprocess_image(img):
-    img = cv2.resize(255 - cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (28, 28))
-    img = img.reshape(1, 28, 28, 1).astype('float32') / 255
-    return img
-
-
-def app():
-    st.title("Digit Recognition")
-    canvas = st_canvas(
-        fill_color="white",
-        stroke_width=20,
-        stroke_color="black",
-        background_color="white",
-        width=200,
-        height=200,
-        drawing_mode="freedraw",
-        key="canvas"
-    )
-
-    if st.button("Predict"):
-        drawn_image = canvas.image_data
-        preprocessed_image = preprocess_image(drawn_image)
-        prediction = model.predict(preprocessed_image)
-        plt.imshow(preprocessed_image.reshape(28, 28), cmap='gray')
-        plt.show()
-        print(prediction)
-        digit_index = np.argmax(prediction)
-        st.write("Predicted Digit:", digit_index)
-
-
-if __name__ == "__main__":
-    app()
-=======
 import streamlit as st
 import numpy as np
 import cv2
@@ -52,18 +7,13 @@ from streamlit_drawable_canvas import st_canvas
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
 
-# Load the Keras model
 model = tf.keras.models.load_model('final.h5')
 
-# Define the canvas size and stroke width
 canvas_width = 300
 canvas_height = 300
 stroke_width = 10
 
-# Define the drawing mode
 drawing_mode = st.sidebar.selectbox("Drawing mode", ["freedraw", "transform"])
-
-# Define a function to preprocess the image for the model
 
 
 def preprocess_image(image):
@@ -74,8 +24,6 @@ def preprocess_image(image):
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
-# Define a function to get a prediction from the model
-
 
 def predict_digit(image):
     image_array = preprocess_image(image)
@@ -83,12 +31,9 @@ def predict_digit(image):
     digit = np.argmax(prediction)
     return digit
 
-# Create the Streamlit app
-
 
 def main():
     st.title("Sketchboard")
-    # Create a canvas for drawing
     canvas = st_canvas(
         fill_color="white",
         stroke_width=stroke_width,
@@ -110,4 +55,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> e04176bd1e9366d02a7265eedf411d89e8c4d58c
